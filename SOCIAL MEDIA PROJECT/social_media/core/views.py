264a794a8549d4e2a9d61ba4f62728f69707d8eb
Sanjay <sanjaydevarajworks@gmail.com>
2026-01-26 19:52:30 +0530

    Creating an files index, profile, setting, signin, signup in templates folder

--- SOCIAL MEDIA PROJECT/social_media/core/views.py
+++ SOCIAL MEDIA PROJECT/social_media/core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to Social media project')
+    return render(request, "index.html")
