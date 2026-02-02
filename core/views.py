da3ae4c9156c36967beb40497a56277345c2b5e5
Sanjay <sanjaydevarajworks@gmail.com>
2026-02-02 12:30:41 +0530

    Adding an feature to profile page

--- core/views.py
+++ core/views.py
@@ -30,26 +30,43 @@ def upload(request):
     return HttpResponse('<h1>Upload Views</h1>')
 
 @login_required(login_url="signin")
+# def like_post(request):
+#     username = request.user.username
+#     post_id = request.GET.get('post_id')
+
+#     post = Post.objects.get(id=post_id)
+
+#     like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+#     if like_filter == 'None':
+#         new_like = LikePost.objects.create(post_id=post_id, username=username)
+#         new_like.save()
+#         post.no_of_likes = post.no_of_likes + 1
+#         post.save()
+#         return redirect('/')
+#     else:
+#         like_filter.delete()
+#         post.no_of_likes = post.no_of_likes - 1
+#         post.save()
+#         return redirect('/')
 def like_post(request):
     username = request.user.username
     post_id = request.GET.get('post_id')
 
     post = Post.objects.get(id=post_id)
 
-    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    like = LikePost.objects.filter(post_id=post_id, username=username).first()
 
-    if like_filter == 'None':
-        new_like = LikePost.objects.create(post_id=post_id, username=username)
-        new_like.save()
-        post.no_of_likes = post.no_of_likes + 1
+    if like is None:
+        LikePost.objects.create(post_id=post_id, username=username)
+        post.no_of_likes += 1
         post.save()
-        return redirect('/')
     else:
-        if like_filter.exists():
-            like_filter.delete()
-            post.no_of_likes = post.no_of_likes - 1
-            post.save()
-            return redirect('/')
+        like.delete()
+        post.no_of_likes -= 1
+        post.save()
+
+    return redirect('/')
 
 
 
